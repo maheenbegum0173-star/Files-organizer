d0d365c90c25b9d5feaa5caa297b8ff80e798574
Maheen Begum <maheen.begum0173@gmail.com>
2026-03-22 23:42:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,5 +66,7 @@ def index():
 
     return render_template("index.html", message=message, stats=stats, files=files)
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
